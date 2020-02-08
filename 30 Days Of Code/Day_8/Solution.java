8e83dc61413c57b27600b3cfa5946dd077858f03
saurav <sauravpd58@gmail.com>
2020-02-08 12:01:17 +0530

    added Day8 solution.java

---
+++ 30 Days Of Code/Day_8/Solution.java
@@ -0,0 +1,32 @@
+package Day_8;
+
+//Complete this code or write your own from scratch
+import java.util.HashMap;
+import java.util.Scanner;
+
+class Solution{
+	public static void main(String []argh){
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		HashMap<String, Integer> map = new HashMap<>();
+		for(int i = 0; i < n; i++){
+			String name = in.next();
+			int phone = in.nextInt();
+			map.put(name, phone);
+		}
+		while(in.hasNext()){
+			String s = in.next();
+			if(s.contains("exit"))
+			{
+				System.out.println("exit...");
+				break;
+			}
+			if (map.containsKey(s)) {
+				System.out.println(s + "=" + map.get(s));
+			} else {
+				System.out.println("Not found");
+			}
+		}
+		in.close();
+	}
+}
