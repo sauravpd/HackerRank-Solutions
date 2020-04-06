00958d1f997e36117ab41db2e59480cb359fdef8
saurav <sauravpd58@gmail.com>
2020-04-07 01:00:23 +0530

    added If Else .. solution1.java

---
+++ Java/ifElse/Solution.java
@@ -0,0 +1,30 @@
+package ifElse;
+
+import java.util.Scanner;
+
+public class Solution 
+{
+	private static final Scanner scanner = new Scanner(System.in);
+
+	public static void main(String[] args) 
+	{
+		int n = scanner.nextInt();
+		scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+		
+		String ans = "";
+		if (n % 2 == 1) {
+			ans = "Weird";
+		} else {
+			if (n >= 6 && n <= 20) {
+				ans = "Weird";
+			} else {
+				ans = "Not Weird";
+			}
+		}
+
+		/* Print output */
+		System.out.println(ans);
+		scanner.close();
+	}
+}
+
