96361bd0030ead1a475eac1e996b95be285fdbe4
saurav <sauravpd58@gmail.com>
2020-04-11 00:18:56 +0530

    added EndOfFile Solution.java

---
+++ Java/EndOfFile/Solution.java
@@ -0,0 +1,19 @@
+package EndOfFile;
+
+import java.util.Scanner;
+
+public class Solution {
+
+	public static void main(String[] args) {
+
+		Scanner sc=new Scanner(System.in);
+		int x=1;
+		while (sc.hasNextLine()) 
+		{
+
+			System.out.println(x + " " +sc.nextLine());
+			x++;
+		}
+		sc.close();
+	}
+}
